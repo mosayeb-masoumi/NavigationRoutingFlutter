948b7aadaa0d00433e0c630bef14c2d29f01ada6
Mosayeb Masoumi <masoumi.projects.co@gmail.com>
2023-05-17 17:30:32 +0430

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,6 @@ class MyApp extends StatelessWidget {
 
   // This widget is the root of your application.
   @override
-
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
@@ -38,7 +37,7 @@ class MyApp extends StatelessWidget {
             return MaterialPageRoute(builder: (context) => HomePage());
         }
       },
-      initialRoute: "/",
+      // initialRoute: "/",
     );
   }
 }
